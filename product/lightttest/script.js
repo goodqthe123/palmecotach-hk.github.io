6e1891614d2c175bf62c41ab5e6bf67e7b0d7b45
JamesKwong <111725853+goodqthe123@users.noreply.github.com>
2023-03-31 14:56:41 +0800

    Add files via upload

---
+++ product/lightttest/script.js
@@ -0,0 +1,14 @@
+// Open the popup box
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+  }
+  
+  // Close the popup box
+  function closePopup() {
+    document.getElementById("popup").style.display = "none";
+  }
+  
+  // Set the carousel image
+  function setCarouselImage(image) {
+    document.getElementById("carousel-image").src = image;
+  }
